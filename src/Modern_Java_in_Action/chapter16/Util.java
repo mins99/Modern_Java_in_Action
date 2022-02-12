e0561f695af8bb46f01274f6d9e458d0909d4809
Celina <luv.mins99@gmail.com>
2022-02-12 14:11:15 +0900

    16.2 비동기 API 구현

---
+++ src/Modern_Java_in_Action/chapter16/Util.java
@@ -0,0 +1,31 @@
+package Modern_Java_in_Action.chapter16;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+public class Util {
+
+    private static final Random RANDOM = new Random(0);
+    private static final DecimalFormat formatter = new DecimalFormat("#.##", new DecimalFormatSymbols(Locale.US));
+
+    public static void delay() {
+        int delay = 1000;
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static double format(double number) {
+        synchronized (formatter) {
+            return new Double(formatter.format(number));
+        }
+    }
+
+}
